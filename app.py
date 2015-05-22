4b495508bd3457db19f7c7cfb207b009c9f0d8bd
dimichelec <dimichelec@users.noreply.github.com>
2015-05-22 11:13:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session
-
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'JDas8dj123e&&@0h';
-
 
 @app.route('/')
 def home():
@@ -12,15 +11,19 @@ def home():
 @app.route('/signup', methods=['POST'])
 def signup():
 	session['username'] = request.form['username']
-	session['message'] = request.form['message']
+	session['comment'] = request.form['comment']
 	return redirect(url_for('message'))
 
 @app.route('/message')
 def message():
 	if not 'username' in session:
 		return abort(403)
-	return render_template('message.html', username=session['username'], message=session['message'])
+	return render_template('message.html',
+		username=session['username'], comment=session['comment'],
+		random=''.join(random.choice('0123456789abcdef') for i in range(256))
+	)
 
 
 if __name__ == "__main__":
+	app.debug = True
 	app.run()
