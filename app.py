4ae246f0f7a39c8e1636ee9402e82754548fe232
dimichelec <dimichelec@users.noreply.github.com>
2015-05-23 01:00:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,66 @@
-from flask import Flask, render_template, request, redirect, url_for, abort, session, jsonify
+from flask import request
+from flask.ext.api import FlaskAPI, status
+import psycopg2
+from psycopg2.extras import RealDictCursor
 import random
+import urlparse
+import json
 
-app = Flask(__name__)
+
+app = FlaskAPI(__name__)
 app.config['SECRET_KEY'] = 'JDas8dj123e&&@0h';
 
-@app.route('/')
-def home():
-	return render_template('index.html')
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse('postgres://dahgrmgyrvnjxh:43OOFe3S4YNQrq3Rsy6__ihdjZ@ec2-107-20-178-83.compute-1.amazonaws.com:5432/d4neua8lk9eb55')
 
-@app.route('/signup', methods=['POST'])
-def signup():
-	session['username'] = request.form['username']
-	session['comment'] = request.form['comment']
-	return redirect(url_for('output'))
+print "\n---Starting app.py"
 
-@app.route('/output')
-def output():
-	if not 'username' in session:
-		return abort(403)
-	return render_template('output.html',
-		username=session['username'], comment=session['comment'],
-		random=''.join(random.choice('0123456789abcdef') for i in range(256)),
-		json=jsonify(indent=2, sort_keys=False, items={
-			'username': session['username'],
-			'comment': session['comment'],
-			'data': ''.join(random.choice('0123456789abcdef') for i in range(256))
-		})
-	)
+db = psycopg2.connect(
+	database=url.path[1:],
+	user=url.username,
+	password=url.password,
+	host=url.hostname,
+	port=url.port
+)
+print " + Opened database successfully"
+cur = db.cursor(cursor_factory=RealDictCursor)
 
-@app.route('/outputjson')
-def outputjson():
-	if not 'username' in session:
-		return abort(403)
-	return jsonify(items={
-		'username': session['username'],
-		'comment': session['comment'],
-		'data': ''.join(random.choice('0123456789abcdef') for i in range(256))
-	})
+cur.execute("CREATE TABLE IF NOT EXISTS dataunits (id SERIAL PRIMARY KEY, username CHAR(20), comment CHAR(100), dataunit CHAR(256));")
+print " + Table created successfully (or already existed)"
+db.commit()
+
+
+@app.route('/', methods=['GET','POST'])
+def dataunits():
+	if request.method == 'POST':
+		cur.execute("INSERT INTO dataunits (username,comment,dataunit) VALUES(%s,%s,%s)", (
+			str(request.data.get('username', '')),
+			str(request.data.get('comment', '')),
+			''.join(random.choice('0123456789abcdef') for i in range(256))
+		))
+		db.commit()
+		cur.execute("SELECT * FROM dataunits")
+		db.commit()
+		return json.dumps(cur.fetchall(), indent=2), status.HTTP_201_CREATED
+		
+	cur.execute("SELECT * FROM dataunits")
+	db.commit()
+	return json.dumps(cur.fetchall(), indent=2)
+
+
+@app.route("/<int:id>/", methods=['GET','DELETE'])
+def dataunit_detail(id):
+	if request.method == 'DELETE':
+		cur.execute("DELETE FROM dataunits WHERE id=%s;",(id,))
+		db.commit()
+		return '', status.HTTP_204_NO_CONTENT
+
+	# request.method == 'GET'
+	cur.execute("SELECT * FROM dataunits WHERE id=%s;",(id,))
+	db.commit()
+	return json.dumps(cur.fetchone(), indent=2)
+
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug = True)
